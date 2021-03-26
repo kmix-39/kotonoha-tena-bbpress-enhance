473d9d5d41c3af7559bd55e7a3daf6582dc314fb
kmix-39 <cheero.energy@gmail.com>
2021-03-26 14:31:04 +0900

    トピックタグ時、検索条件が正しく機能しない問題の対応 - #7

--- App/Part/AdvanceForumSearch/Bootstrap.php
+++ App/Part/AdvanceForumSearch/Bootstrap.php
@@ -16,7 +16,7 @@ class Bootstrap {
 
 		$_is_load = false;
 		// フォーラム時対応
-		$_is_load |= bbp_is_forum_archive() || bbp_is_topic_archive();
+		$_is_load |= bbp_is_forum_archive() || bbp_is_topic_archive() || bbp_is_topic_tag();
 		// 検索キーワードを入力されていない場合、検索が見つからない場合
 		$_is_load |= ( bbp_is_search() && ! bbp_has_search_results() && ! bbp_get_search_terms() );
 		// 検索結果がない場合でもフォームを出す場合
@@ -25,7 +25,7 @@ class Bootstrap {
 		// 検索結果一覧でフォームが有効な場合
 		Helper::get_settings_value( 'result-display-search-form', false ) &&
 			$_is_load |= ( bbp_is_search() );
-		
+
 		if ( $_is_load ) {
 			Helper::enqueue_scripts_ktpp_bbpress_enhance( 'search' );
 			Helper::enqueue_styles_ktpp_bbpress_enhance( 'search' );
